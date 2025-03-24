177978e773f8412b9371b8f6f6440b03b77c29e9
tizianotambone <tambonetiziano@gmail.com>
2025-03-24 14:37:10 +0100

    [UPD]ex2 ok

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali(names){
+     let arrayIniziali =[];
+    for (let i = 0; i < names.length; i++) {
+        arrayIniziali.push(names[i].charAt(0))
+    }
+
+      return arrayIniziali  
+        
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
